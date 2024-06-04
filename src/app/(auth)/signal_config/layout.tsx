d0da11ceec21948964e78dbb3771be6002cff7df
164sumit <164sumit20@gmail.com>
2024-06-04 18:20:28 +0530

    "signal_config layout"

--- src/app/(auth)/signal_config/layout.tsx
+++ src/app/(auth)/signal_config/layout.tsx
@@ -1,7 +1,9 @@
 import Navbar from "@/components/Navbar";
 import { Sidebar } from "@/components/Sidebar";
 
-export default function DashboardLayout({ children }) {
+export default function DashboardLayout({ children }: {
+  children: React.ReactNode
+}) {
   return (
     <section className="flex flex-col-2 gap-4 p-4 flex-grow  " style={{ height: 'calc(100vh - 4rem)'}}>
       {/* Include shared UI here e.g. a header or sidebar */}
