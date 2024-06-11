027cf97ab6aa46ee23cae7db21a3efeb19b19e01
Sumit <@xyz>
2024-06-11 15:52:01 +0530

    "dsdd"

--- src/app/temp1/page.tsx
+++ src/app/temp1/page.tsx
@@ -6,6 +6,7 @@ import React from 'react'
 const page = () => {
   return (
     <div>
+      <h1>experimental </h1>
         <AddsensorButton/>
         <PickleToJsonConverter/>
         <ExcelToJsonComponent/>
