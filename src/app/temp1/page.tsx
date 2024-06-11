c99e40233c1ba2dd8400979f29c30e470a2519f0
Sumit <129009419+164sumit@users.noreply.github.com>
2024-06-11 15:58:46 +0530

    "diok"

--- src/app/temp1/page.tsx
+++ src/app/temp1/page.tsx
@@ -6,6 +6,7 @@ import React from 'react'
 const page = () => {
   return (
     <div>
+      <h1 classname="bg-blue" >dsdsds</h1>
         <AddsensorButton/>
         <PickleToJsonConverter/>
         <ExcelToJsonComponent/>
