ecedd44c4e3819a89e76a30e679e521aa796b7c6
164sumit <164sumit20@gmail.com>
2024-06-18 10:22:55 +0530

    "change logs"

--- src/app/logs/page.tsx
+++ src/app/logs/page.tsx
@@ -1,4 +1,5 @@
 "use client"
+
 import * as React from "react"
 import {
   ColumnDef,
@@ -12,8 +13,7 @@ import {
   getSortedRowModel,
   useReactTable,
 } from "@tanstack/react-table"
-import { ArrowUpDown, CalendarIcon, ChevronDown, MoreHorizontal } from "lucide-react"
-import { saveAs } from 'file-saver'
+import { ArrowUpDown, ChevronDown, MoreHorizontal } from "lucide-react"
 
 import { Button } from "@/components/ui/button"
 import { Checkbox } from "@/components/ui/checkbox"
@@ -35,126 +35,145 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table"
-import { getlogs } from "@/actions/logs.action"
-import { Calendar } from "@/components/ui/calendar"
-import { cn } from "@/lib/utils"
-import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover"
-import { format } from "date-fns"
 
-export type LogEntry = {
-    srNo?: number
-    message: string
-    timestamp: string
-    level?: string
-    owner?: string
-}
-
-export const columns: ColumnDef<LogEntry>[] = [
-    {
-      accessorKey: "srNo",
-      header: "Sr.No",
-      cell: ({ row }) => <div>{row.getValue("srNo")}</div>,
-    },
-    {
-      accessorKey: "message",
-      header: ({ column }) => {
-        return (
-          <Button
-            variant="ghost"
-            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-          >
-            Message
-            <ArrowUpDown className="ml-2 h-4 w-4" />
-          </Button>
-        )
-      },
-      cell: ({ row }) => <div>{row.getValue("message")}</div>,
-    },
-    {
-      accessorKey: "owner",
-      header: ({ column }) => {
-        return (
-          <Button
-            variant="ghost"
-            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-          >
-            Owner
-            <ArrowUpDown className="ml-2 h-4 w-4" />
-          </Button>
-        )
-      },
-      cell: ({ row }) => <div>{row.getValue("owner")}</div>,
-    },
-    {
-      accessorKey: "timestamp",
-      header: ({ column }) => {
-        return (
-          <Button
-            variant="ghost"
-            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
-          >
-            Timestamp
-            <ArrowUpDown className="ml-2 h-4 w-4" />
-          </Button>
-        )
-      },
-      cell: ({ row }) => <div>{new Date(row.getValue("timestamp")).toLocaleString()}</div>,
-    },
-    
+const data: Payment[] = [
+  {
+    id: "m5gr84i9",
+    amount: 316,
+    status: "success",
+    email: "ken99@yahoo.com",
+  },
+  {
+    id: "3u1reuv4",
+    amount: 242,
+    status: "success",
+    email: "Abe45@gmail.com",
+  },
+  {
+    id: "derv1ws0",
+    amount: 837,
+    status: "processing",
+    email: "Monserrat44@gmail.com",
+  },
+  {
+    id: "5kma53ae",
+    amount: 874,
+    status: "success",
+    email: "Silas22@gmail.com",
+  },
+  {
+    id: "bhqecj4p",
+    amount: 721,
+    status: "failed",
+    email: "carmella@hotmail.com",
+  },
 ]
 
-const DataTableDemo = () => {
+export type Payment = {
+  id: string
+  amount: number
+  status: "pending" | "processing" | "success" | "failed"
+  email: string
+}
+
+export const columns: ColumnDef<Payment>[] = [
+  {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        checked={
+          table.getIsAllPageRowsSelected() ||
+          (table.getIsSomePageRowsSelected() && "indeterminate")
+        }
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
+  {
+    accessorKey: "status",
+    header: "Status",
+    cell: ({ row }) => (
+      <div className="capitalize">{row.getValue("status")}</div>
+    ),
+  },
+  {
+    accessorKey: "email",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Email
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      )
+    },
+    cell: ({ row }) => <div className="lowercase">{row.getValue("email")}</div>,
+  },
+  {
+    accessorKey: "amount",
+    header: () => <div className="text-right">Amount</div>,
+    cell: ({ row }) => {
+      const amount = parseFloat(row.getValue("amount"))
+
+      // Format the amount as a dollar amount
+      const formatted = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+      }).format(amount)
+
+      return <div className="text-right font-medium">{formatted}</div>
+    },
+  },
+  {
+    id: "actions",
+    enableHiding: false,
+    cell: ({ row }) => {
+      const payment = row.original
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={() => navigator.clipboard.writeText(payment.id)}
+            >
+              Copy payment ID
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>View customer</DropdownMenuItem>
+            <DropdownMenuItem>View payment details</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      )
+    },
+  },
+]
+
+const DataTableDemo=()=> {
   const [sorting, setSorting] = React.useState<SortingState>([])
-  const [data, setData] = React.useState<LogEntry[]>([])
-  const [selectedOwner, setSelectedOwner] = React.useState<string>('all')
-  const [owners, setOwners] = React.useState<string[]>([])
-  const [startDate, setStartDate] = React.useState<Date>()
-  const [endDate, setEndDate] = React.useState<Date>()
-
-  React.useEffect(() => {
-    // Fetch logs when the component mounts or when the selected owner changes
-    const fetchData = async () => {
-      try {
-        const logs = await getlogs();
-        const logEntries: LogEntry[] = logs.map((log, index) => ({
-          srNo: index + 1,
-          message: log.message,
-          timestamp: log.timestamp,
-          level: log.level,
-          owner: log.owner,
-        }));
-
-        // Extract unique owners for the select box
-        const uniqueOwners = Array.from(new Set(logEntries.map(log => log.owner || 'default-owner')));
-        setOwners(uniqueOwners);
-
-        let filteredLogs = logEntries;
-        if (selectedOwner !== 'all') {
-          filteredLogs = filteredLogs.filter(log => log.owner === selectedOwner);
-        }
-        if (startDate) {
-          filteredLogs = filteredLogs.filter(log => new Date(log.timestamp) >= startDate);
-        }
-        if (endDate) {
-          const endFilterDate = new Date(endDate);
-        endFilterDate.setHours(23, 59, 59, 999);
-          filteredLogs = filteredLogs.filter(log => new Date(log.timestamp) <= endFilterDate);
-        }
-        setData(filteredLogs);
-      } catch (error) {
-        console.error('Error fetching logs:', error);
-      }
-    };
-
-    fetchData();
-  }, [selectedOwner, startDate, endDate]);
-
-  const handleOwnerChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    setSelectedOwner(event.target.value);
-  };
-  
-  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>([])
-  const [columnVisibility, setColumnVisibility] = React.useState<VisibilityState>({})
+  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
+    []
+  )
+  const [columnVisibility, setColumnVisibility] =
+    React.useState<VisibilityState>({})
   const [rowSelection, setRowSelection] = React.useState({})
 
   const table = useReactTable({
@@ -176,83 +195,17 @@ const DataTableDemo = () => {
     },
   })
 
-  const handleDownload = () => {
-    const logContent = data.map(log => JSON.stringify({
-      level: log.level,
-      message: log.message,
-      timestamp: log.timestamp,
-      owner: log.owner,
-    })).join('\n');
-
-    const blob = new Blob([logContent], { type: 'text/plain;charset=utf-8' });
-    saveAs(blob, 'combined.log');
-  }
-  
   return (
     <div className="w-full">
       <div className="flex items-center py-4">
         <Input
-          placeholder="Filter message..."
-          value={(table.getColumn("message")?.getFilterValue() as string) ?? ""}
+          placeholder="Filter emails..."
+          value={(table.getColumn("email")?.getFilterValue() as string) ?? ""}
           onChange={(event) =>
-            table.getColumn("message")?.setFilterValue(event.target.value)
+            table.getColumn("email")?.setFilterValue(event.target.value)
           }
           className="max-w-sm"
         />
-        <select onChange={handleOwnerChange} value={selectedOwner} className="ml-4 p-2 border rounded">
-          <option value="all">All Owners</option>
-          {owners.map(owner => (
-            <option key={owner} value={owner}>{owner}</option>
-          ))}
-        </select>
-        <div className="ml-4">
-          <Popover>
-            <PopoverTrigger asChild>
-              <Button
-                variant={"outline"}
-                className={cn(
-                  "w-[140px] justify-start text-left font-normal",
-                  !startDate && "text-muted-foreground"
-                )}
-              >
-                <CalendarIcon className="mr-2 h-4 w-4" />
-                {startDate ? format(startDate, "PPP") : <span>Start Date</span>}
-              </Button>
-            </PopoverTrigger>
-            <PopoverContent className="w-auto p-0">
-              <Calendar
-                mode="single"
-                selected={startDate}
-                onSelect={setStartDate}
-                initialFocus
-              />
-            </PopoverContent>
-          </Popover>
-        </div>
-        <div className="ml-4">
-          <Popover>
-            <PopoverTrigger asChild>
-              <Button
-                variant={"outline"}
-                className={cn(
-                  "w-[140px] justify-start text-left font-normal",
-                  !endDate && "text-muted-foreground"
-                )}
-              >
-                <CalendarIcon className="mr-2 h-4 w-4" />
-                {endDate ? format(endDate, "PPP") : <span>End Date</span>}
-              </Button>
-            </PopoverTrigger>
-            <PopoverContent className="w-auto p-0">
-              <Calendar
-                mode="single"
-                selected={endDate}
-                onSelect={setEndDate}
-                initialFocus
-              />
-            </PopoverContent>
-          </Popover>
-        </div>
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
             <Button variant="outline" className="ml-auto">
@@ -279,9 +232,6 @@ const DataTableDemo = () => {
               })}
           </DropdownMenuContent>
         </DropdownMenu>
-        <Button variant="outline" className="ml-4" onClick={handleDownload}>
-          Download Logs
-        </Button>
       </div>
       <div className="rounded-md border">
         <Table>
@@ -322,7 +272,10 @@ const DataTableDemo = () => {
               ))
             ) : (
               <TableRow>
-                <TableCell colSpan={columns.length} className="h-24 text-center">
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
                   No results.
                 </TableCell>
               </TableRow>
@@ -332,13 +285,24 @@ const DataTableDemo = () => {
       </div>
       <div className="flex items-center justify-end space-x-2 py-4">
         <div className="flex-1 text-sm text-muted-foreground">
-          {table.getFilteredRowModel().rows.length} row(s).
+          {table.getFilteredSelectedRowModel().rows.length} of{" "}
+          {table.getFilteredRowModel().rows.length} row(s) selected.
         </div>
         <div className="space-x-2">
-          <Button variant="outline" size="sm" onClick={() => table.previousPage()} disabled={!table.getCanPreviousPage()}>
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.previousPage()}
+            disabled={!table.getCanPreviousPage()}
+          >
             Previous
           </Button>
-          <Button variant="outline" size="sm" onClick={() => table.nextPage()} disabled={!table.getCanNextPage()}>
+          <Button
+            variant="outline"
+            size="sm"
+            onClick={() => table.nextPage()}
+            disabled={!table.getCanNextPage()}
+          >
             Next
           </Button>
         </div>
